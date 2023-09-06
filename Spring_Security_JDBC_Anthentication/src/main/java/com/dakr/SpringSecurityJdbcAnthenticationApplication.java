796a6f4565d0af0c0390bc86fa809c028d745167
sukanya <sukanyaakula90@gmail.com>
2023-09-06 15:09:11 +0530

    Spring Boot JDBC Authentication

---
+++ Spring_Security_JDBC_Anthentication/src/main/java/com/dakr/SpringSecurityJdbcAnthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.dakr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcAnthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcAnthenticationApplication.class, args);
+	}
+
+}
